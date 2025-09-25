349c4464de17a3e3588e9282f11cd1b5b7948b83
AmnSingh452 <amnsingh1102@gmail.com>
2025-09-26 01:08:13 +0530

    Fix Prisma import in abandoned cart discount API
    - Changed from named import {prisma} to default import prisma
    - Resolves build error: prisma is not exported by db.server.js
    - Build now completes successfully
    - Cart abandonment feature should deploy properly

--- app/routes/api.abandoned-cart-discount.jsx
+++ app/routes/api.abandoned-cart-discount.jsx
@@ -1,6 +1,6 @@
 import { json } from "@remix-run/node";
 import { authenticate } from "../shopify.server";
-import { prisma } from "../db.server";
+import prisma from "../db.server";
 
 export const action = async ({ request }) => {
   try {
